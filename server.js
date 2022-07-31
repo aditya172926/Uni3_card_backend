5ed6bda85cb3eb9a74084be9be9b9d3d60927117
Aditya <aditya26sg@gmail.com>
2022-07-31 14:09:53 +0530

    update

--- server.js
+++ server.js
@@ -7,15 +7,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin, X-Requested, Content-Type, Accept, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE, OPTIONS"
-    )
-    return res.status(200).json({})
-  }
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next()
 });
 
